c13ec90de69febe9bff3a99d79195790691c6817
Andre153 <andreluiz153pro@gmail.com>
2021-08-03 12:51:59 -0300

    finalizando

---
+++ single-fire-components-vue-2/src/components/NovoJogo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <!--<img src="../assets/america_mg_60x60.png" alt="">-->
+        <button class="btn btn-primary" @click="criarNovoJogo" ref="button">Novo jogo</button>
+        <placar-modal :time-casa="timeCasa" :time-fora="timeFora" ref="modal"></placar-modal>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                timeCasa: null,
+                timeFora: null,
+                times: this.timesColecao
+            }
+        },
+        //props: ['times'],
+        inject: ['timesColecao'],
+        methods: {
+            criarNovoJogo() {
+                console.log(this.$refs);
+                var indiceCasa = Math.floor(Math.random() * 20),
+                    indiceFora = Math.floor(Math.random() * 20);
+
+                this.timeCasa = this.timesColecao[indiceCasa];
+                this.timeFora = this.timesColecao[indiceFora];
+                var modal = this.$refs.modal;
+                console.log(modal);
+                modal.showModal();
+            },
+        }
+    }
+</script>
